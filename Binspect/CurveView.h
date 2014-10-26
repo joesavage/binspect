7ee73e589980abdfeba353f2a051255b56e51b28
Joe Savage <joe@reinterpretcast.com>
2014-10-26 02:47:02 +0000

    remove NSScrollView, add basic OpenGL drawing through NSOpenGLView subclass

---
+++ Binspect/CurveView.h
@@ -0,0 +1,18 @@
+//
+//  CurveView.h
+//  Binspect
+//
+//  Created by Joe Savage on 26/10/2014.
+//  Copyright (c) 2014 Joe Savage. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface CurveView : NSOpenGLView
+{
+    
+}
+
+- (void) drawRect: (NSRect) bounds;
+
+@end
---
+++ Binspect/CurveView.m
@@ -0,0 +1,44 @@
+//
+//  CurveView.m
+//  Binspect
+//
+//  Created by Joe Savage on 26/10/2014.
+//  Copyright (c) 2014 Joe Savage. All rights reserved.
+//
+
+#import "CurveView.h"
+#include <OpenGL/gl.h>
+
+@implementation CurveView
+
+- (void) prepareOpenGL {
+    // NSLog(@"Preparing...");
+}
+
+-(void) drawRect: (NSRect) bounds
+{
+    glClearColor(0, 0, 0, 0);
+    glClear(GL_COLOR_BUFFER_BIT);
+    drawAnObject();
+    glFlush();
+}
+
+static void drawAnObject ()
+{
+    glColor3f(1.0f, 0.85f, 0.35f);
+    glBegin(GL_TRIANGLES);
+    {
+        glVertex3f(  0.0,  0.6, 0.0);
+        glVertex3f( -0.2, -0.3, 0.0);
+        glVertex3f(  0.2, -0.3 ,0.0);
+    }
+    glEnd();
+}
+
+- (void) scrollWheel: (NSEvent*) event {
+    // NSLog(@"Scroll!");
+}
+
+// For mouse hovering in future: updateTrackingAreas, mouseEntered, mouseExited
+
+@end
