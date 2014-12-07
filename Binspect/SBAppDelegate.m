6390be7b52cd12e9d3f2f596eb93ed4c88bf6158
Joe Savage <joe@reinterpretcast.com>
2014-12-07 15:00:41 +0000

    modify code headers

--- Binspect/SBAppDelegate.m
+++ Binspect/SBAppDelegate.m
@@ -3,7 +3,6 @@
 //  Binspect
 //
 //  Created by Joe Savage on 18/10/2014.
-//  Copyright (c) 2014 Joe Savage. All rights reserved.
 //
 
 #import "SBAppDelegate.h"
--- Binspect/SBCurveView.h
+++ Binspect/SBCurveView.h
@@ -3,7 +3,6 @@
 //  Binspect
 //
 //  Created by Joe Savage on 26/10/2014.
-//  Copyright (c) 2014 Joe Savage. All rights reserved.
 //
 
 #import <Cocoa/Cocoa.h>
